40a8ec67d268461432ba55c5b6f1efe4e7729d32
Salahudeen-3301 <swnafee1187@gmail.com>
2025-12-30 12:26:37 +0400

    Update batch_track.py

--- Model/batch_track.py
+++ Model/batch_track.py
@@ -1,75 +1,61 @@
+
+
 from ultralytics import YOLO
-import os
+import cv2
+import math
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 1056)
+cap.set(4, 672)
 
-# --- Load your trained YOLOv11 model ---
-model = YOLO(r"C:\Users\swnaf\Downloads\yolo11-stl\trained_models\yolo_weights\model8.pt")
+# model
+model = YOLO("yolo11n.pt")
+frame_num = 0
+frame_jump = 6
 
-# --- Paths ---
-video_folder = r"C:\Users\swnaf\OneDrive\Desktop\Model_Videos\test\with_people\Resized1056"          # folder containing all videos
-  # resized videos
-output_folder = r"C:\Users\swnaf\Downloads\yolo11-stl\runs\full23"  # where results go
-tracker_type = r"C:\Users\swnaf\Downloads\yolo11-stl\trained_models\bytetrack.yaml"       # tracker config
+# object classes
+classNames = ["person"]
 
-os.makedirs(output_folder, exist_ok=True)
-
-# --- Track results summary ---
-min_conf = 0.2
-min_frames = 8
-videos_with_people = 0
-total_videos = 0
-
-for video_name in os.listdir(video_folder):
-    number = 0
-    confidences = []
-    found = False
-    if not video_name.lower().endswith(('.mp4', '.avi', '.mov', '.mkv')):
+while True:
+    frame_num+=1
+    success, img = cap.read()
+    if frame_num%frame_jump !=0:
         continue
 
-    total_videos += 1
-    video_path = os.path.join(video_folder, video_name)
-    print(f"\n🎥 Processing: {video_name}")
+    results = model.track(img, stream=False, imgsz=(672,1056), classes=[0])
 
-    # Run tracking
-    results = model.track(
-        source=video_path,
-        save=True,
-        tracker=tracker_type,
-        show=False,
-        imgsz=(672,1056),
-        project=output_folder,
-        name="Track",           # prevents subfolders
-        exist_ok=True,
-        persist=False,
-        stream=True,
-        verbose=False
-    )
+    # coordinates
+    for r in results:
+        boxes = r.boxes
 
-    # Collect unique IDs
-    unique_ids = set()
-    for result in results:
-        boxes = getattr(result, "boxes", None)
-        if boxes is not None and (boxes.id is not None or boxes.conf is not None):
-            if boxes.id is not None:
-                ids = boxes.id.cpu().numpy()
-                unique_ids.update(ids)
-            else:
-                for conf in result.boxes.conf:
-                    conf=conf.item()
-                    # print(conf)
-                    if conf >= min_conf:
-                        number += 1
+        for box in boxes:
+            cls = int(box.cls[0])
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-    # Print results for this video
-    if len(unique_ids) == 0 and  number < min_frames:
-        print(f"⚠️  No people detected in {video_name}")
-    else:
-        print(f"✅ {len(unique_ids)} people detected in {video_name}")
-        videos_with_people += 1
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
 
-# --- Final summary ---
-print("\n📊 Final Summary:")
-print(f"Total videos processed: {total_videos}")
-print(f"Videos with people detected: {videos_with_people}")
-print(f"Videos with no detections: {total_videos - videos_with_people}")
-print("\n✅ All videos processed successfully!")
+            # class name
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
